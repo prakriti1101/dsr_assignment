69be09054b89c24a3525104f9478ce0ee57d5e14
prakriti1101 <you@example.com>
2023-08-29 15:46:47 +0530

    it2

--- arm_ws/src/doosan-robot/dsr_control/scripts/plan_target_position.py
+++ arm_ws/src/doosan-robot/dsr_control/scripts/plan_target_position.py
@@ -22,21 +22,21 @@ move_group =  moveit_commander.MoveGroupCommander(group_name)
 display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory,queue_size=20)
 
 
-# pose_target = geometry_msgs.msg.Pose()
-# pose_target.orientation.w =  1.0
-# pose_target.position.x = 0.5
-# pose_target.position.y = 0.0
-# pose_target.position.z  = 0.2 
-# move_group.set_pose_target(pose_target)
+pose_target = geometry_msgs.msg.Pose()
+pose_target.orientation.w =  1.0
+pose_target.position.x = 0.5
+pose_target.position.y = 0.0
+pose_target.position.z  = 0.2 
+move_group.set_pose_target(pose_target)
 
 # # plan1 = move_group.plan()
 # # rospy.sleep(5)
 # # moveit_commander.roscpp_shutdown
 
-# # `go()` returns a boolean indicating whether the planning and execution was successful.
-# success = move_group.go(wait=True)
-# # Calling `stop()` ensures that there is no residual movement
-# move_group.stop()
-# # It is always good to clear your targets after planning with poses.
-# # Note: there is no equivalent function for clear_joint_value_targets().
-# move_group.clear_pose_targets()
+# `go()` returns a boolean indicating whether the planning and execution was successful.
+success = move_group.go(wait=True)
+# Calling `stop()` ensures that there is no residual movement
+move_group.stop()
+# It is always good to clear your targets after planning with poses.
+# Note: there is no equivalent function for clear_joint_value_targets().
+move_group.clear_pose_targets()
